a66870dc8ccce2426f0dd548f82f520c4ab07db5
ameliaetsa <118045809+ameliaetsa@users.noreply.github.com>
2023-01-02 13:27:12 +0700

    Add files via upload

---
+++ ug11.py
@@ -0,0 +1,22 @@
+#no1
+print('PRAKTIKUM TEKNOLOGI KOMPUTER')
+print('Silakan diganti dengan nama anda')
+print('Berikut ini merupakan contoh program penjumlahan')
+print('5')
+
+#no2
+a= int(input("bilangan pertama= "))
+b= int(input("bilangan kedua= "))
+totaltambah= a+b
+totalkali= a*b
+print("hasil penjumlahan semua bilangan = ",totaltambah)
+print("hasil perkalian semua bilangan = ",totalkali)
+ditambah= totaltambah+0.5
+dikali= totalkali*0.5
+print("Jika ditambah 0.5= ",ditambah)
+print("jika dikali 0.5= ",dikali)
+
+#no3
+a = int(input('Masukkan panjang alas : '))
+t = int(input('Masukkan tinggi segitiga : '))
+print('Luas segitiga adalah :', a*t/2)
---
+++ ug12.py
@@ -0,0 +1,22 @@
+# #no1
+# a = input('Masukkan nama Anda : ')
+# if a == 'Bond':
+#     print('Selamat datang agen 007')
+# else : 
+#     print('Selamat Pagi', a)
+
+# #no2
+# b = int(input('Masukkan Angka : '))
+# if b%2 == 0:
+#     print('Genap')
+# else :
+#     print('Ganjil')
+
+#no3
+c = input('Masukkan Angka pecahan : ')
+if ('.' or ',')in c: 
+    print("Integer")
+else: 
+    print("Bilangan bulat")
+
+
